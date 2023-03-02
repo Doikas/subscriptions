a2f6dbda6c0495f08b21167de575270709a86fa2
Doikas <stevekdoikas@hotmail.com>
2023-03-02 11:43:09 +0200

    Design Home,Login

---
+++ resources/views/brand/header.blade.php
@@ -0,0 +1,13 @@
+@push('head')
+    <link
+        href="/favicon.ico"
+        id="favicon"
+        rel="icon"
+    >
+@endpush
+<p class="h2 n-m font-thin v-center">
+    <img src="/favicon.ico" alt="">
+    <span class="m-l d-none d-sm-block" style="padding-left:11px;">
+       Wdesign
+    </span>
+</p>

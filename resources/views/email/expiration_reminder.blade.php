c191d8ee594ef1acf52b584aa0d37a81b13c12aa
Doikas <stevekdoikas@hotmail.com>
2023-09-11 09:55:06 +0300

    Change date format and email template

--- resources/views/email/expiration_reminder.blade.php
+++ resources/views/email/expiration_reminder.blade.php
@@ -6,15 +6,31 @@
     <title>Service Expiration Reminder</title>
 </head>
 <body>
-    <div>
-        <p>Dear {{ $customer_pronunciation }},</p>
-        <p>Your service for {{ $service_name }} is expiring soon. Please take note of the following details:</p>
-        <ul>
-            <li>Service Name: {{ $service_name }}</li>
-            <li>Expired Date: {{ $expired_date }}</li>
-        </ul>
-        <p>Please contact us for payment options to renew your service.</p>
-        <p>Thank you!</p>
-    </div>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+            <td align="center">
+                <table width="600" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse;">
+                    <tr>
+                        <td align="center" bgcolor="#f5f5f5" style="padding: 40px 0;">
+                            <img src="images/WDesign logo.png" alt="wdesignLogo" style="max-width: 150px;">
+                            <h1 style="color: #333333;">Service Expiration Reminder</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#ffffff" style="padding: 20px;">
+                            <p>Dear {{ $customer_pronunciation }},</p>
+                            <p>Your service for {{ $service_name }} is set to expire on {{ $expired_date }}.</p>
+                            <p>Please contact us for payment details to renew your service.</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" bgcolor="#f5f5f5" style="padding: 20px;">
+                            <p style="color: #999999; font-size: 12px;">This is an automated message. Please do not reply.</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
